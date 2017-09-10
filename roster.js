d2b917b9ad92f4688330523e6507fda341fe206c
jasonbaker <jason.th.baker@gmail.com>
2017-09-10 03:10:50 -0500

    Commit

--- roster.js
+++ roster.js
@@ -1,18 +1,22 @@
 var teamMembers =
     {"Colin Bromley":
-        {photo:"<img-src=resources/colin.jpg",
-        text: "Colin is originally from Pennsylvania and has been racing bikes in Central Texas for 5 years"
-        }
+        {"photo":"<img src='resources/photos/colinbromley.jpg'>",
+        "text":"Colin is originally from Pennsylvania and has been racing bikes in Central Texas for 5 years"
+        },
      "Jason Baker":
-        {photo:"<img-src=resources/jason.jpg",
-        text: "Jason is from Austin and has been racing for 4 years."
-        }
-    }
+        {"photo":"<img src='resources/photos/jasonbaker.jpg'>",
+        "text":"Jason is from Austin and has been racing for 4 years."
+        },
+     "Michael Orsow":{"text":"Mike is a total baller from Oregon."},
+    };
 
-function $(document).ready(function(){
-    $("#content").click(function(event){
-        var fullid = $(this).attr(id);
-        $("button").show(teamMembers.fullid.photo);
-        $("button").show(teamMembers.fullid.text);
-    });
+$.each(teamMembers, function(key){
+	$("#NameButtons").append("<li>"+key+"</li>");
+	});
+
+$(document).ready(function(){
+    $("#NameButtons li").click(function(){
+      var fullid = $(this).text();
+      $("#content").replaceWith('<div id="content"><p>'+teamMembers[fullid].text+"</p></div>");
+      });
 });
